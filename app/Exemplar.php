fc0876f33172715d8dedd175a707ce58f68db2c0
JoaoPaulo333 <joaopaulo2008@gmail.com>
2018-11-26 05:19:51 -0200

    model e migration Exemplar

---
+++ app/Exemplar.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Exemplar extends Model
+{
+    private $table = "Exemplar";
+}
---
+++ database/migrations/2018_11_26_051343_create_exemplars_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateExemplarsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('exemplar', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('disponivel')->default(1);
+            $table->string('arquivo');
+            $table->integer('Livro_id');
+            $table->foreign('Livro_id')->references('id')->on("Livro")->onDelete('cascade');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('exemplar');
+    }
+}
