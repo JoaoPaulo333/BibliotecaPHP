80270f21e3d47fad66f209e2af0feaf82a0d3b2c
JoaoPaulo333 <joaopaulo2008@gmail.com>
2018-11-26 05:30:04 -0200

    model e migration Reserva

---
+++ database/migrations/2018_11_26_052543_create_reservas_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReservasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reserva', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('data');
+            $table->integer('Livro_id')->unsigned();
+            $table->foreign('Livro_id')->references('id')->on("Livro")->onDelete('cascade');
+            $table->integer('Usuario_id');
+            $table->foreign('Usuario_id')->references('id')->on("Usuario")->onDelete('cascade');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reserva');
+    }
+}
